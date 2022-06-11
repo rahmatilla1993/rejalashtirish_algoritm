3dab0848460df47a6cbd749ac4fcc8a2facd8744
rahmatilla1993 <rahmatilla301993@gmail.com>
2022-06-11 19:28:18 +0500

    Dinamik prioritet

--- src/Algorithm/Second.js
+++ src/Algorithm/Second.js
@@ -14,9 +14,13 @@ export default function waterWheel(arr, tk, P, dev, expel) {
   arr = JSON.parse(JSON.stringify(arr));
   const matrix = [[]];
   const waiter = {};
+  const dynamicCounter = {};
   arr.forEach((el) =>
     matrix[0].push(el.name) &&
     Object.assign(waiter, {
+      [el.name]: 0
+    }) &&
+    Object.assign(dynamicCounter, {
       [el.name]: 0
     })
   );
@@ -48,12 +52,13 @@ export default function waterWheel(arr, tk, P, dev, expel) {
         }
       }
     }
-
+    let once = true;
     //haydab chiqarish
     if (expel)
       for (let q of a) {
 
         if (q.begin === i - 1) {
+          once = true;
           for (let value of worker) {
             if (value.work === q.name) {
               break
@@ -64,10 +69,13 @@ export default function waterWheel(arr, tk, P, dev, expel) {
           })
 
           for (let b = 0; b < worker.length; b++) {
-            if (arr[worker[b].work.charCodeAt() - 97].begin !== q.begin && arr[worker[b].work.charCodeAt() - 97].priority > q.priority) {
+            if (arr[worker[b].work.charCodeAt() - 97].begin !== q.begin &&
+              arr[worker[b].work.charCodeAt() - 97].priority > q.priority &&
+              once
+            ) {
               worker[b].work = q.name;
               worker[b].curr = 0;
-              break;
+              once = false;
             }
           }
         }
@@ -83,6 +91,13 @@ export default function waterWheel(arr, tk, P, dev, expel) {
           continue;
         }
         matrix[i][j.work.charCodeAt() - 97] += dev ? j.name.charAt(2) : 'B';
+        dynamicCounter[j.work.charCodeAt() - 97] =
+          dynamicCounter[j.work.charCodeAt() - 97] <= 0 ?
+            1 : 2;
+        if (dynamicCounter[j.work.charCodeAt() - 97] === 2) {
+          dynamicCounter[j.work.charCodeAt() - 97] = 0;
+          arr[j.work.charCodeAt() - 97].priority += 1;
+        }
         j.curr += 1;
         arr[j.work.charCodeAt() - 97].resource -= 1;
         if (j.curr === j.max || arr[j.work.charCodeAt() - 97].resource <= 0) {
@@ -95,6 +110,13 @@ export default function waterWheel(arr, tk, P, dev, expel) {
     for (const k of a) {
       if (!matrix[i][k.name.charCodeAt() - 97]) {
         matrix[i][k.name.charCodeAt() - 97] = 'K';
+        dynamicCounter[k.name.charCodeAt() - 97] =
+          dynamicCounter[k.name.charCodeAt() - 97] >= 0 ?
+            -1 : -2;
+        if (dynamicCounter[k.name.charCodeAt() - 97] === -2) {
+          dynamicCounter[k.name.charCodeAt() - 97] = 0;
+          arr[k.name.charCodeAt() - 97].priority -= 1;
+        }
         waiter[k.name] = 0;
       }
     }
